d089c833872df83e6fa6d8a76eb35ffbfb68aeaf
Moin93 <Moinmirmasomi@outlook.com>
2021-12-09 11:01:09 +0100

    added menubuttons to set gamemode.

--- src/GuiFrame.java
+++ src/GuiFrame.java
@@ -1,10 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GuiFrame extends JFrame {
-
+public class GuiFrame extends JFrame implements ActionListener {
+    JMenu menu;
+    JMenuItem i1, i2;
     public GuiFrame(Player player1, Player player2) {
         super("FYRA-I-RAD");
+        JMenuBar mb=new JMenuBar();
+        menu=new JMenu("GAME MODE");
+        i1=new JMenuItem("two player");
+        i2=new JMenuItem("one player");
+        i1.addActionListener(this);
+        i2.addActionListener(this);
+        menu.add(i1); menu.add(i2);
+        mb.add(menu);
+        menu.setBackground(Color.black);
+        setJMenuBar(mb);
         setPreferredSize(new Dimension(1000, 800));
         setVisible(true);
         setResizable(true);
@@ -13,6 +26,11 @@ public class GuiFrame extends JFrame {
         add(board);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
