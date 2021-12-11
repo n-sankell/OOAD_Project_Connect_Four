03dc6b269d2951cc45f29eaabc18b7f158d52b33
Henrik Sjöstrand <61562647+henriksjstrand@users.noreply.github.com>
2021-12-11 14:26:45 +0100

    changed color on JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import static javax.swing.UIManager.put;
 public class Main {
 
     public static void main(String[] args) {
-        put("OptionPane.background", Color.BLACK);
+        put("OptionPane.background", Color.WHITE);
         put("Panel.background", Color.WHITE);
         put("OptionPane.messageFont", new Font("Druk Wide", Font.BOLD, 14));
         put("OptionPane.buttonFont", new Font("Druk Wide", Font.PLAIN, 12));
