32d84e9710595033e16cb8f0e355e28a24f078bd
Henrik Sjöstrand <61562647+henriksjstrand@users.noreply.github.com>
2021-12-11 14:28:03 +0100

    changed font size on JoptionPane

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         put("OptionPane.background", Color.WHITE);
         put("Panel.background", Color.WHITE);
-        put("OptionPane.messageFont", new Font("Druk Wide", Font.BOLD, 14));
+        put("OptionPane.messageFont", new Font("Druk Wide", Font.BOLD, 12));
         put("OptionPane.buttonFont", new Font("Druk Wide", Font.PLAIN, 12));
         new GuiFrame();
     }
