e3bf32bad7f6db58896c56b2c67c5ead90c15d6a
Niklas Sankell <niklas_san@gmail.com>
2022-01-10 00:07:00 +0100

    renamed some variables

--- src/GUI/CustomJop.java
+++ src/GUI/CustomJop.java
@@ -9,19 +9,19 @@ import java.io.IOException;
 
 public class CustomJop {
 
-    private static Dialog d;
+    private static Dialog dialog;
 
     public CustomJop(String message) throws IOException {
-        JFrame f = new JFrame();
-        d = new JDialog(f , true);
-        d.setLayout( new BorderLayout());
+        JFrame frame = new JFrame();
+        dialog = new JDialog(frame , true);
+        dialog.setLayout( new BorderLayout());
 
-        JButton b = new JButton (new ImageIcon("ASSETS/okButton.png"));
-        b.addActionListener (e -> CustomJop.d.setVisible(false));
-        b.setBounds(520,600,200,80);
+        JButton button = new JButton (new ImageIcon("ASSETS/okButton.png"));
+        button.addActionListener (e -> CustomJop.dialog.setVisible(false));
+        button.setBounds(520,600,200,80);
 
         JLabel label = new JLabel(new ImageIcon("ASSETS/scoreBoard.png"));
-        d.add (label,BorderLayout.CENTER);
+        dialog.add (label,BorderLayout.CENTER);
 
         JTextPane score = new JTextPane();
         score.setBounds(386,300,450,600);
@@ -29,17 +29,16 @@ public class CustomJop {
         score.setText(message);
         score.setForeground(GuiColors.BOARD);
         score.setFont(new Font("Druk Wide",Font.BOLD,30));
-
         StyledDocument documentStyle = score.getStyledDocument();
         SimpleAttributeSet centerAttribute = new SimpleAttributeSet();
         StyleConstants.setAlignment(centerAttribute, StyleConstants.ALIGN_CENTER);
         documentStyle.setParagraphAttributes(0, documentStyle.getLength(), centerAttribute, false);
 
-        label.add(b);
+        label.add(button);
         label.add(score);
 
-        d.setSize(1000,800);
-        d.setVisible(true);
+        dialog.setSize(1000,800);
+        dialog.setVisible(true);
 
     }
 }
